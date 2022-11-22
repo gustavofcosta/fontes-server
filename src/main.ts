3281f13af37708be044828d8d0c95d082a077ff0
gustavofcosta <gfcdesenvolvimento@gmail.com>
2022-11-22 14:56:40 -0300

    instalando prisma ORM

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('API Fontes')
+    .setTitle('API Fontes')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
